4306d0fdbcc62812968ce7f585ca4eeaf75e8399
profh <profh@cmu.edu>
2012-03-14 15:24:06 -0400

    Missing _id in custom validation error messages

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -40,14 +40,14 @@ class Assignment < ActiveRecord::Base
   def employee_is_active_in_system
     all_active_employees = Employee.active.all.map{|e| e.id}
     unless all_active_employees.include?(self.employee_id)
-      errors.add(:employee, "is not an active employee at the creamery")
+      errors.add(:employee_id, "is not an active employee at the creamery")
     end
   end
   
   def store_is_active_in_system
     all_active_stores = Store.active.all.map{|s| s.id}
     unless all_active_stores.include?(self.store_id)
-      errors.add(:store, "is not an active store at the creamery")
+      errors.add(:store_id, "is not an active store at the creamery")
     end
   end
 end
